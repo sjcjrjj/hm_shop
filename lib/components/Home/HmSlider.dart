e58cd84532822c97d2ff5c10c93450fc312afefc
sjcjrjj <sjcjrjjj@gmail.com>
2026-08-21 04:48:31 +0800

    实现轮播图渲染效果

--- lib/components/Home/HmSlider.dart
+++ lib/components/Home/HmSlider.dart
@@ -1,20 +1,42 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:hm_shop/viewmodels/home.dart';
 
 class HmSlider extends StatefulWidget {
-  const HmSlider({super.key});
+  final List<BannerItem> bannerList;
+  const HmSlider({super.key, required this.bannerList});
 
   @override
   State<HmSlider> createState() => _HmSliderState();
 }
 
 class _HmSliderState extends State<HmSlider> {
+  Widget _getSlider() {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    return CarouselSlider(items: List.generate(widget.bannerList.length, 
+    (int index) {
+      return Image.network(widget.bannerList[index].imgurl,
+      fit: BoxFit.cover,
+      width: screenWidth,
+      );
+    }),
+     options: CarouselOptions(
+      height: 300,
+      viewportFraction: 1,
+      autoPlay: true,
+      autoPlayInterval: Duration(seconds: 1),
+     ));
+  }
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-      color: Colors.blue,
-      alignment: Alignment.center,
-      child: Text("轮播图", style: TextStyle(color:Colors.white, fontSize: 20),),
+    return Stack(
+      children: [_getSlider()],
     );
+    // return Container(
+    //   height: 300,
+    //   color: Colors.blue,
+    //   alignment: Alignment.center,
+    //   child: Text("轮播图", style: TextStyle(color:Colors.white, fontSize: 20),),
+    // );
   }
 }
---
+++ lib/viewmodels/home.dart
@@ -0,0 +1,6 @@
+
+class BannerItem {
+  String id;
+  String imgurl;
+  BannerItem({required this.id, required this.imgurl});
+}
